998adc8a81030a66a17caf5b226f83ac3afdbb34
yatima99 <tanigawayusuke496@gmail.com>
2023-12-11 14:08:36 +0900

    Postモデルの実装

---
+++ rails/db/migrate/20231211042544_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :title, comment: "タイトル"
+      t.text :content, comment: "本文"
+      t.integer :status, comment: "ステータス（10:未保存, 20:下書き, 30:公開中）"
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
