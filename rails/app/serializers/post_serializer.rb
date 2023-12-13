299c09d17e968f399cf5c6ca4d94213c66b71d4f
yatima99 <tanigawayusuke496@gmail.com>
2023-12-13 22:23:54 +0900

    マイ記事の新規作成・更新アクションの実装

--- rails/app/serializers/post_serializer.rb
+++ rails/app/serializers/post_serializer.rb
@@ -1,8 +1,12 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :created_at
+  attributes :id, :title, :content, :status, :created_at
   belongs_to :user, serializer: UserSerializer
 
   def created_at
     object.created_at.strftime("%Y/%m/%d")
+  end
+
+  def status
+    object.status_i18n
   end
 end
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
       namespace :current do
         resource :user, only: [:show]
+        resources :posts, only: [:create, :update]
       end
       resources :posts, only: [:index, :show]
     end
