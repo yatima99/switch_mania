5b49483d5b074a642e4841953d7ed2903e155023
yatima99 <tanigawayusuke496@gmail.com>
2023-12-15 00:24:57 +0900

    記事一覧ページの実装

--- rails/spec/requests/api/v1/current/posts_spec.rb
+++ rails/spec/requests/api/v1/current/posts_spec.rb
@@ -1,57 +1,54 @@
 require "rails_helper"
 
 RSpec.describe "Api::V1::Current::Posts", type: :request do
-  describe "GET api/v1/current/posts" do
-    subject { get(api_v1_current_posts_path, headers:) }
+  describe "POST api/v1/current/posts" do
+    subject { post(api_v1_current_posts_path, headers:) }
 
     let(:headers) { current_user.create_new_auth_token }
     let(:current_user) { create(:user) }
-    let(:other_user) { create(:user) }
 
-    before do
-      create_list(:post, 2, user: other_user)
-    end
-
-    context "ログインユーザーに紐づく posts レコードが存在する時" do
-      before do
-        create_list(:post, 3, user: current_user)
-      end
-
-      it "正常にレコードを取得できる" do
-        subject
+    context "ログインユーザーに紐づく未保存ステータスの記事が0件の時" do
+      it "未保存ステータスの記事が新規作成される" do
+        expect { subject }.to change { current_user.posts.count }.by(1)
+        expect(current_user.posts.last).to be_unsaved
         res = JSON.parse(response.body)
-        expect(res.length).to eq 3
-        expect(res[0].keys).to eq ["id", "title", "content", "status", "created_at", "updated_at", "user"]
-        expect(res[0]["user"].keys).to eq ["name"]
+        expect(res.keys).to eq ["id", "title", "content", "status", "created_at", "user"]
+        expect(res["user"].keys).to eq ["name"]
         expect(response).to have_http_status(:ok)
       end
     end
 
-    context "ログインユーザーに紐づく posts レコードが存在しない時" do
-      it "空の配列が返る" do
-        subject
+    context "ログインユーザーに紐づく未保存ステータスの記事が1件の時" do
+      before { create(:post, user: current_user, status: :unsaved) }
+
+      it "未保存ステータスの記事が新規作成される" do
+        expect { subject }.not_to change { current_user.posts.count }
         res = JSON.parse(response.body)
-        expect(res).to eq []
+        expect(res.keys).to eq ["id", "title", "content", "status", "created_at", "user"]
+        expect(res["user"].keys).to eq ["name"]
         expect(response).to have_http_status(:ok)
       end
     end
   end
 
-  describe "GET api/v1/current/posts/:id" do
-    subject { get(api_v1_current_post_path(id), headers:) }
+  describe "PATCH api/v1/current/posts" do
+    subject { patch(api_v1_current_post_path(id), headers:, params:) }
 
     let(:headers) { current_user.create_new_auth_token }
     let(:current_user) { create(:user) }
     let(:other_user) { create(:user) }
+    let(:params) { { "post": { "title": "テストタイトル2", "content": "テスト本文2", "status": "published" } } }
 
     context ":id がログインユーザーに紐づく posts レコードの id である時" do
-      let(:current_user_post) { create(:post, user: current_user) }
+      let(:current_user_post) { create(:post, title: "テストタイトル1", content: "テスト本文1", status: :draft, user: current_user) }
       let(:id) { current_user_post.id }
 
-      it "正常にレコードを取得できる" do
-        subject
+      it "正常にレコードを更新できる" do
+        expect { subject }.to change { current_user_post.reload.title }.from("テストタイトル1").to("テストタイトル2") and
+          change { current_user_post.reload.content }.from("テスト本文1").to("テスト本文2") and
+          change { current_user_post.reload.status }.from("draft").to("published")
         res = JSON.parse(response.body)
-        expect(res.keys).to eq ["id", "title", "content", "status", "created_at", "updated_at", "user"]
+        expect(res.keys).to eq ["id", "title", "content", "status", "created_at", "user"]
         expect(res["user"].keys).to eq ["name"]
         expect(response).to have_http_status(:ok)
       end
