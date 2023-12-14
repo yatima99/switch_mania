293d315b54dd5ffad1da008aa9b4f500c9a6cca7
yatima99 <tanigawayusuke496@gmail.com>
2023-12-15 01:58:21 +0900

    サインインページの実装

--- rails/config/initializers/cors.rb
+++ rails/config/initializers/cors.rb
@@ -7,10 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "Settings.front_domain"
+    origins Settings.front_domain
 
     resource "*",
              headers: :any,
+             expose: ['access-token', 'uid','client'],
              methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
