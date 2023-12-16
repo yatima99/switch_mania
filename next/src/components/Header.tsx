7bcd001addadda7950de94412ebbca559daf08b1
yatima99 <tanigawayusuke496@gmail.com>
2023-12-17 01:32:24 +0900

    サインアウト機能を実装

--- next/src/components/Header.tsx
+++ next/src/components/Header.tsx
@@ -1,8 +1,41 @@
-import { AppBar, Box, Button, Container } from '@mui/material'
+import ArticleIcon from '@mui/icons-material/Article'
+import Logout from '@mui/icons-material/Logout'
+import PersonIcon from '@mui/icons-material/Person'
+import SearchIcon from '@mui/icons-material/Search'
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Divider,
+  IconButton,
+  Menu,
+  MenuItem,
+  ListItemIcon,
+  Typography,
+} from '@mui/material'
+
+import InputBase from '@mui/material/InputBase'
+import Paper from '@mui/material/Paper'
 import Image from 'next/image'
 import Link from 'next/link'
 
+import { useState } from 'react'
+import { useUserState } from '@/hooks/useGlobalState'
+
 const Header = () => {
+  const [user] = useUserState()
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
   return (
     <AppBar
       position="static"
@@ -26,37 +59,114 @@ const Header = () => {
               <Image src="/logo.png" width={133} height={40} alt="logo" />
             </Link>
           </Box>
-          <Box>
-            <Link href="/sign_in">
-              <Button
-                color="primary"
-                variant="contained"
-                sx={{
-                  color: 'white',
-                  textTransform: 'none',
-                  fontSize: 16,
-                  borderRadius: 2,
-                  boxShadow: 'none',
-                }}
-              >
-                Sign in
-              </Button>
-            </Link>
-            <Button
-              color="primary"
-              variant="outlined"
-              sx={{
-                textTransform: 'none',
-                fontSize: 16,
-                borderRadius: 2,
-                boxShadow: 'none',
-                border: '1.5px solid #3EA8FF',
-                ml: 2,
-              }}
-            >
-              Sign Up
-            </Button>
-          </Box>
+          <Paper
+            component="form"
+            sx={{
+              p: '2px 4px',
+              display: 'flex',
+              alignItems: 'center',
+              width: 400,
+            }}
+          >
+            <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+              <SearchIcon />
+            </IconButton>
+            <InputBase
+              sx={{ ml: 1, flex: 1 }}
+              placeholder="Search"
+              inputProps={{ 'aria-label': 'search google maps' }}
+            />
+          </Paper>
+          {user.isFetched && (
+            <>
+              {!user.isSignedIn && (
+                <Box>
+                  <Link href="/sign_in">
+                    <Button
+                      color="primary"
+                      variant="contained"
+                      sx={{
+                        color: 'white',
+                        textTransform: 'none',
+                        fontSize: 16,
+                        borderRadius: 2,
+                        boxShadow: 'none',
+                      }}
+                    >
+                      Sign in
+                    </Button>
+                  </Link>
+                  <Button
+                    color="primary"
+                    variant="outlined"
+                    sx={{
+                      textTransform: 'none',
+                      fontSize: 16,
+                      borderRadius: 2,
+                      boxShadow: 'none',
+                      border: '1.5px solid #3EA8FF',
+                      ml: 2,
+                    }}
+                  >
+                    Sign Up
+                  </Button>
+                </Box>
+              )}
+              {user.isSignedIn && (
+                <Box sx={{ display: 'flex' }}>
+                  <IconButton onClick={handleClick} sx={{ p: 0 }}>
+                    <Avatar>
+                      <PersonIcon />
+                    </Avatar>
+                  </IconButton>
+                  <Box sx={{ ml: 2 }}>
+                    <Button
+                      color="primary"
+                      variant="contained"
+                      sx={{
+                        color: 'white',
+                        textTransform: 'none',
+                        fontSize: 16,
+                        borderRadius: 2,
+                        width: 100,
+                        boxShadow: 'none',
+                      }}
+                    >
+                      Add new
+                    </Button>
+                  </Box>
+                  <Menu
+                    anchorEl={anchorEl}
+                    id="account-menu"
+                    open={open}
+                    onClose={handleClose}
+                    onClick={handleClose}
+                  >
+                    <Box sx={{ pl: 2, py: 1 }}>
+                      <Typography sx={{ fontWeight: 'bold' }}>
+                        {user.name}
+                      </Typography>
+                    </Box>
+                    <Divider />
+                    <MenuItem>
+                      <ListItemIcon>
+                        <ArticleIcon fontSize="small" />
+                      </ListItemIcon>
+                      投稿の管理
+                    </MenuItem>
+                    <Link href="/sign_out">
+                      <MenuItem>
+                        <ListItemIcon>
+                          <Logout fontSize="small" />
+                        </ListItemIcon>
+                        サインアウト
+                      </MenuItem>
+                    </Link>
+                  </Menu>
+                </Box>
+              )}
+            </>
+          )}
         </Box>
       </Container>
     </AppBar>
