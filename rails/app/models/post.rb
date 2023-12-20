48e86ce57b5b707ffd54c48afaa355abf0c64b19
yatima99 <tanigawayusuke496@gmail.com>
2023-12-20 21:54:47 +0900

    音声投稿機能(rails)

--- rails/app/models/post.rb
+++ rails/app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   mount_uploader :image, ImageUploader
+  mount_uploader :audio, AudioUploader
   enum :status, { unsaved: 10, draft: 20, published: 30 }, _prefix: true
   validates :title, :content, presence: true, if: :published?
   validate :verify_only_one_unsaved_status_is_allowed
--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -4,7 +4,9 @@ ActiveRecord::Base.transaction do
   user2 = User.create!(name: "テスト次郎", email: "test2@example.com", password: "password", confirmed_at: Time.current)
 
   15.times do |i|
-    Post.create!(title: "テストタイトル1-#{i}", content: "テスト本文1-#{i}", image: File.open("./public/test.png"), status: :published, user: user1)
-    Post.create!(title: "テストタイトル2-#{i}", content: "テスト本文2-#{i}", image: File.open("./public/hhkb.jpg"), status: :published, user: user2)
+    Post.create!(title: "テストタイトル1-#{i}", content: "テスト本文1-#{i}", image: File.open("./public/test.jpg"), audio: File.open("./public/hana.mp3"),
+                 status: :published, user: user1)
+    Post.create!(title: "テストタイトル2-#{i}", content: "テスト本文2-#{i}", image: File.open("./public/hhkb.jpg"), audio: File.open("./public/kiniro.mp3"),
+                 status: :published, user: user2)
   end
 end
