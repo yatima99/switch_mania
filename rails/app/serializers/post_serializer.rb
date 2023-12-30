9d92b8c6b64e69e8f1df61e72138de23b41918fa
yatima99 <tanigawayusuke496@gmail.com>
2023-12-30 18:19:10 +0900

    いいね機能の実装(next)

--- rails/app/serializers/post_serializer.rb
+++ rails/app/serializers/post_serializer.rb
@@ -1,5 +1,7 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :content, :status, :created_at, :image, :audio
+  attribute :liked, if: -> { additional_info.present? }
+  attribute :like_id, if: -> { additional_info.present? }
   belongs_to :user, serializer: UserSerializer
 
   def created_at
@@ -9,4 +11,18 @@ class PostSerializer < ActiveModel::Serializer
   def status
     object.status_i18n
   end
+
+  def liked
+    additional_info[:liked] if additional_info
+  end
+
+  def like_id
+    additional_info[:like_id] if additional_info
+  end
+
+  private
+
+    def additional_info
+      @instance_options[:additional_info] || {}
+    end
 end
